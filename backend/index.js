f973f07e34c28a20d08ca57b386f7e2fc5641e95
Monah-Haidar <mnhaidar01@gmail.com>
2024-05-15 03:11:22 +0300

    Backend <> all functionality related to the trading journal routes are done

--- backend/index.js
+++ backend/index.js
@@ -208,6 +208,8 @@ app.put("/journals/:journal_id", async (req, res) => {
     const queryString = `UPDATE journals SET ${fields.join(
       ", "
     )} WHERE journal_id = $${fields.length + 1} RETURNING *;`;
+
+
     console.log(queryString);
     console.log(values);
 
@@ -330,12 +332,66 @@ app.post("/journals/journal_entries", async (req, res) => {
 // update journal entries
 app.put("/journals/journal_entries/:entry_id", async (req, res) => {
   const {entry_id} = req.params;
-  const {journal_id, trade_id, title, content, market_conditions, mood, img, entry_date} = req.body;
+  const {title, content, market_conditions, mood, img, entry_date} = req.body;
+
+  if (isNaN(parseInt(entry_id))) {
+    return res.json("Invalid entry ID");
+  }
+ 
   try {
 
-    
+    if (!title && !content && !market_conditions && !mood && !img && !entry_date){
+      return res.json("No update fields provided.");
+    }
+
+    const fields = [];
+    const values = [];
 
 
+    if (title) {
+      fields.push("title = $" + (fields.length + 1));
+      values.push(title);
+    }
+    if (content) {
+      fields.push("content = $" + (fields.length + 1));
+      values.push(content);
+    }
+    if (market_conditions) {
+      fields.push("market_conditions = $" + (fields.length + 1));
+      values.push(market_conditions);
+    }
+    if (mood) {
+      fields.push("mood = $" + (fields.length + 1));
+      values.push(mood);
+    }
+    if (img) {
+      fields.push("img = $" + (fields.length + 1));
+      values.push(img);
+    }
+    if (entry_date) {
+      fields.push("entry_date = $" + (fields.length + 1));
+      values.push(entry_date);
+    }
+
+    if (fields.length === 0) {
+      return res.json("No valid fields provided for update.");
+    }
+
+    values.push(entry_id);
+
+    const queryString = `UPDATE journal_entries SET ${fields.join(", ")} WHERE entry_id = $${fields.length + 1} RETURNING *;` ;
+    console.log(queryString);
+    console.log('values', values);
+    const result = await pool.query(queryString, values);
+
+    if (result.rows.length === 0) {
+      return res.json("Journal entries not found or no change made.");
+    }
+    res.json({
+      message: 'Journal Entry Updated Successfully',
+      data: result.rows
+    })
+      
   } catch (error) {
     console.error(error);
     res.json("Server error");
