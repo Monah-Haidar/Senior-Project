177befc94759fb3b231d0267ead3abf286c4f804
Monah-Haidar <mnhaidar01@gmail.com>
2024-05-16 04:18:19 +0300

    Journal feature complete

--- backend/controllers/jounalController.js
+++ backend/controllers/jounalController.js
@@ -5,17 +5,18 @@ import jwt from "jsonwebtoken";
 const createJournal = async (req, res) => {
   try {
     await Journal.sync();
-    const {title, description} = req.body;
-    const existingJournal = await Journal.findOne({where:{title}});
+    const { title, description } = req.body;
+    const existingJournal = await Journal.findOne({ where: { title } });
 
-    if (existingJournal != null){
-      return res.status(400).json({message: "Journal Already Exists"});
+    if (existingJournal != null) {
+      return res.status(400).json({ message: "Journal Already Exists" });
     }
 
-    const journal = await Journal.create({title, description, user_id});
+    const journal = await Journal.create({ title, description, user_id });
 
-    return res.status(200).json({message: "Journal Created Successfully", data: journal});
-
+    return res
+      .status(200)
+      .json({ message: "Journal Created Successfully", data: journal });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
@@ -27,16 +28,13 @@ const getJournal = async (req, res) => {
     const journal_id = req.params.id;
     const journal = await Journal.findByPk(journal_id);
 
-    if (!journal){
-      return res.status(404).json({"message": "Journal not found"});
+    if (!journal) {
+      return res.status(404).json({ message: "Journal not found" });
     }
 
-    return res.status(200).json({status:"Journal Retrieved Successfully",
-      data: journal
-    })
-
-
-
+    return res
+      .status(200)
+      .json({ status: "Journal Retrieved Successfully", data: journal });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -44,11 +42,16 @@ const getJournal = async (req, res) => {
 
 const getAllJournals = async (req, res) => {
   try {
+    const journals = await Journal.findAll();
 
+    if (!journals) {
+      return res.status(404).json({ message: "No Journals found" });
+    }
 
-
-
-
+    return res.status(200).json({
+      status: "Success",
+      data: journals,
+    });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -56,11 +59,20 @@ const getAllJournals = async (req, res) => {
 
 const updateJournal = async (req, res) => {
   try {
+    const journal_id = req.params.id;
+    const { title, description } = req.body;
 
+    const journal = await Journal.findByPk(journal_id);
 
+    if (!journal) {
+      return res.status(404).json({ message: "Journal not found" });
+    }
 
+    journal.update({ title, description });
 
-
+    return res
+      .status(200)
+      .json({ status: "Journal Updated Successfully", data: journal });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
   }
@@ -71,12 +83,12 @@ const deleteJournal = async (req, res) => {
     const journal_id = req.params.id;
     const journal = await Journal.findByPk(journal_id);
 
-    if(!journal){
-      return res.status(404).json({"message":"Journal not found"});
+    if (!journal) {
+      return res.status(404).json({ message: "Journal not found" });
     }
 
     await journal.destroy();
-    return res.status(200).json({"message":"Journal deleted"})
+    return res.status(200).json({ message: "Journal deleted" });
   } catch (err) {
     res.status(500).json({ message: "Internal Server Error" });
   }
