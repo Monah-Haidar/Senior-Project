dd56c6370923b6d895367f1a0f84f7c2b533ab9d
tsjalkh <99384692+tsjalkh@users.noreply.github.com>
2024-05-16 20:50:11 +0300

    Delete backend directory

--- backend/routes/news.js
+++
@@ -1,8 +0,0 @@
-// import { getNewsDataFromAPI } from "../controllers/newsController.js";
-// import express from 'express';
-
-// const router = express.Router();
-
-// router.get('/', getNewsDataFromAPI);
-
-// export default router;
