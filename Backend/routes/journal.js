d17e1a3d6dada744684c66f8bc13054bf7de6fef
tsjalkh <99384692+tsjalkh@users.noreply.github.com>
2024-05-16 21:03:16 +0300

    backend

---
+++ Backend/routes/journal.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {createJournal, getJournal, getAllJournals, updateJournal, deleteJournal} from '../controllers/jounalController.js';
+import authenticateUser from '../middleware/authenticateUser.js';
+
+
+const router = express.Router();
+
+router.get('/:id', getJournal);
+router.get('/', authenticateUser,getAllJournals);
+
+router.post('/create', createJournal);
+
+router.put('/:id', updateJournal);
+router.delete('/:id', deleteJournal);
+
+export default router;
